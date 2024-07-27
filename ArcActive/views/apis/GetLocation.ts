8d404d56a4b17c3efb71558456533eca23519fcd
MartinHou <yhou@esri.com>
2024-07-27 10:38:07 -0700

    [feat] data: Get location data

--- ArcActive/views/apis/GetLocation.ts
+++ ArcActive/views/apis/GetLocation.ts
@@ -1,13 +1,17 @@
-import GetLocation from 'react-native-get-location'
+import Geolocation from '@react-native-community/geolocation';
 
-GetLocation.getCurrentPosition({
-    enableHighAccuracy: true,
-    timeout: 60000,
-})
-.then(location => {
-    console.log(location);
-})
-.catch(error => {
-    const { code, message } = error;
-    console.warn(code, message);
-})
+export async function fetchLocation() {
+  return new Promise((resolve, reject) => {
+    Geolocation.getCurrentPosition(
+      location => {
+        resolve({latitude: location.coords.latitude, longitude: location.coords.longitude});
+      },
+      error => {
+        console.error('Error fetching location:', error);
+        reject(null);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000}
+    );
+  });
+}
+
