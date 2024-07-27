1177d7549827f65f9cdb2857275763eacee068e6
Daniel Gerhardt <dgerhardt@esri.com>
2024-07-27 08:18:21 -0700

    working login page

--- ArcActive/app/_layout.tsx
+++ ArcActive/app/_layout.tsx
@@ -2,10 +2,11 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import LoginScreen from '../components/login';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -15,6 +16,7 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+  const [loggedIn, setLoggedIn] = useState(false); // State initialization inside the component
 
   useEffect(() => {
     if (loaded) {
@@ -26,12 +28,14 @@ export default function RootLayout() {
     return null;
   }
 
-  return (
+  return loggedIn ? (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
+  ) : (
+    <LoginScreen setLoggedIn={setLoggedIn} />
   );
 }
