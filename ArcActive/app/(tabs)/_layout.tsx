65dbaa334021c8f05e85282d21f7609551cca97b
Daniel Gerhardt <dgerhardt@esri.com>
2024-07-26 18:31:07 -0700

    adjusted layout and map page

--- ArcActive/app/(tabs)/_layout.tsx
+++ ArcActive/app/(tabs)/_layout.tsx
@@ -24,6 +24,15 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="map"
+        options={{
+          title: 'Map',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'map' : 'map-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
